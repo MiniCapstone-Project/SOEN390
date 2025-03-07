5ef7714bddf0c27cf780e7b1d356278fe30964d8
Sarah <41saraho@gmail.com>
2025-03-06 20:00:26 -0500

    testing smartlook installation, program causing android to crash

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
 import { registerRootComponent } from 'expo';
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar,Platform } from 'react-native';
 import Navigation from '../navigation/Navigation';
 
 import { SMARTLOOK_API_KEY } from '@/constants/SmartlookKey';
 import Smartlook from 'react-native-smartlook-analytics'
 
-Smartlook.instance.preferences.setProjectKey(
-  SMARTLOOK_API_KEY 
-);
-Smartlook.instance.start();
+if (['android', 'ios'].includes(Platform.OS)) {
+  console.log(Platform.OS)
+  import('react-native-smartlook-analytics').then((Smartlook) => {
+      Smartlook.default.instance.preferences.setProjectKey(
+        SMARTLOOK_API_KEY 
+      );
+      console.log(SMARTLOOK_API_KEY)
+      console.log(Smartlook)
+      Smartlook.default.instance.start();
+  });
+}
 
 export default function App() {
   return (
