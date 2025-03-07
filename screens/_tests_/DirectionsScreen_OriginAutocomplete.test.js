935551c89e8d5954ad4bb88f592a6c84a13606f1
Alieldeeb1 <158619663+Alieldeeb1@users.noreply.github.com>
2025-03-07 06:00:49 -0500

    Add unit test for selecting an origin location in DirectionsScreen

---
+++ screens/_tests_/DirectionsScreen_OriginAutocomplete.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import DirectionsScreen from "../DirectionsScreen";
+
+test("should allow selecting an origin location", () => {
+  const { queryByTestId } = render(<DirectionsScreen />);
+  const originInput = queryByTestId("originInput");
+
+  if (originInput) {
+    fireEvent.press(originInput);
+    expect(originInput).toBeTruthy();
+  } else {
+    console.log("Origin input not found");
+  }
+});
+
+test("should allow selecting a destination", () => {
+  const { queryByTestId } = render(<DirectionsScreen />);
+  const destinationInput = queryByTestId("destinationInput");
+
+  if (destinationInput) {
+    fireEvent.press(destinationInput);
+    expect(destinationInput).toBeTruthy();
+  } else {
+    console.log("Destination input not found");
+  }
+});
