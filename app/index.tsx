805d8e22088f728d30050b8a45059ca30a615dba
SarOhayon <135247936+SarOhayon@users.noreply.github.com>
2025-03-09 20:02:48 -0400

    Add Constant Import

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import { registerRootComponent } from 'expo';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import Navigation from '../navigation/Navigation';
+import { SMARTLOOK_API_KEY } from "@/constants/SmartlookKey"; 
 
 export default function App() {
   return (
