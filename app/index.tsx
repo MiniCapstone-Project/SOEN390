e6c1201f701d8ef8b86a286b7d2befdbfd018680
Elion Abdyli <53652464+elion-abdyli@users.noreply.github.com>
2025-03-09 20:39:58 -0400

    fix import statements and add commented out useEffect Smartlook setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
 import { registerRootComponent } from 'expo';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import Navigation from '../navigation/Navigation';
-import { SMARTLOOK_API_KEY } from "@/constants/SmartlookKey"; 
-import Smartlook from 'react-native-smartlook-analytics'
+import { SMARTLOOK_API_KEY } from '@/constants/SmartlookKey'; 
+import Smartlook from 'react-native-smartlook-analytics' ;
 
 export default function App() {
+  // useEffect(() => {
+  //   Smartlook.instance.preferences.setProjectKey(SMARTLOOK_API_KEY);
+  // }, []);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
