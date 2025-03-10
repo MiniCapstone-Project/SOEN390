1918608ce93588bf019871d588b2939e271c48c0
Alieldeeb1 <158619663+Alieldeeb1@users.noreply.github.com>
2025-03-09 21:19:03 -0400

    Add unit test for changing transport mode in DirectionsScreen

--- screens/_tests_/DirectionsScreen_ChangeTransportMode.test.js
+++ screens/_tests_/DirectionsScreen_ChangeTransportMode.test.js
@@ -2,8 +2,43 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 import DirectionsScreen from "../DirectionsScreen";
 
+// Mock external dependencies
+jest.mock("react-native-maps", () => {
+  const React = require("react");
+  const { View } = require("react-native");
+  return {
+    __esModule: true,
+    default: (props) => React.createElement(View, props),
+    PROVIDER_GOOGLE: "google",
+    Marker: (props) => React.createElement(View, props),
+    MapViewDirections: (props) => React.createElement(View, props),
+  };
+});
+
+jest.mock("react-native-google-places-autocomplete", () => {
+  const React = require("react");
+  const { View } = require("react-native");
+  return {
+    __esModule: true,
+    default: () => React.createElement(View, {}),
+  };
+});
+
+jest.mock("@react-navigation/native", () => ({
+  useRoute: jest.fn(() => ({
+    params: {
+      destination: {
+        Address: "Loyola Campus",
+        Latitude: 45.4581244,
+        Longitude: -73.6391259,
+      },
+    },
+  })),
+}));
+
 test("should change transport mode to Walking", () => {
   const { queryByText } = render(<DirectionsScreen />);
+
   const walkingButton = queryByText("Walking");
 
   if (walkingButton) {
@@ -16,6 +51,7 @@ test("should change transport mode to Walking", () => {
 
 test("should change transport mode to Driving", () => {
   const { queryByText } = render(<DirectionsScreen />);
+
   const drivingButton = queryByText("Driving");
 
   if (drivingButton) {
