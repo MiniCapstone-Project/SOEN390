6d8bcc4c5c7c9bdd674046838a98c7e092f111b0
Ziad-Tari <tarikziad347@gmail.com>
2025-04-03 11:54:44 -0400

    adding the adnroid folder but ingoring the androidManifest.xml

---
+++ android/app/.cxx/Debug/6dp2j321/prefab/x86/prefab/lib/i686-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
@@ -0,0 +1,18 @@
+if(NOT TARGET react-native-reanimated::reanimated)
+add_library(react-native-reanimated::reanimated SHARED IMPORTED)
+set_target_properties(react-native-reanimated::reanimated PROPERTIES
+    IMPORTED_LOCATION "C:/GitHub/SOEN390/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/o414356m/obj/x86/libreanimated.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/GitHub/SOEN390/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET react-native-reanimated::worklets)
+add_library(react-native-reanimated::worklets SHARED IMPORTED)
+set_target_properties(react-native-reanimated::worklets PROPERTIES
+    IMPORTED_LOCATION "C:/GitHub/SOEN390/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/o414356m/obj/x86/libworklets.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/GitHub/SOEN390/node_modules/react-native-reanimated/android/build/prefab-headers/worklets"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
