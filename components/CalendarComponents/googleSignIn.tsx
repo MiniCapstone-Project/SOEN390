0ef248dcad08475d0903c64cd9ac22ec7517d893
Ziad-Tari <tarikziad347@gmail.com>
2025-04-03 09:54:06 -0400

    Including the google sign in button in the Setting screen

--- components/CalendarComponents/googleSignIn.tsx
+++ components/CalendarComponents/googleSignIn.tsx
@@ -1,3 +1,4 @@
+import { WEB_CLIENT_ID } from '@/constants/GoogleKey';
 import {
     GoogleSignin,
     GoogleSigninButton,
@@ -9,21 +10,22 @@ import {
 
 
   GoogleSignin.configure({
+    webClientId: WEB_CLIENT_ID,
     scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
     offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
     forceCodeForRefreshToken: false, // [Android] related to `serverAuthCode`, read the docs link below *.
   });
 
 
-  export default function GoogleSignin () {
+  export default function GoogleSign () {
 return (
 
 <View>
-    <Text></Text>
+    <Text>I AM BACKKKKK</Text>
     <GoogleSigninButton
     size={GoogleSigninButton.Size.Wide}
     color={GoogleSigninButton.Color.Dark}/>
-    <StatusBar></StatusBar>
+    <StatusBar ></StatusBar>
 </View>
 );
   }
